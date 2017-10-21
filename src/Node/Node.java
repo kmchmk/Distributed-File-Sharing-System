496986512ac30624c8fc9e13681def270487aab5
kmchmk1026 <kmchmk@gmail.com>
2017-10-21 21:24:57 +0530

    Node class created

---
+++ src/Node/Node.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Node;
+
+import static UPDSocket.Server.echo;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+/**
+ *
+ * @author Chanaka
+ */
+public class Node {
+
+    public void init() {
+
+    }
+
+    public void joinNetwork() {
+        System.out.println("connecting to bootstrap server...");
+    }
+
+    public void listen() {
+        new Thread() {
+            public void run() {
+
+                DatagramSocket sock = null;
+
+                try {
+                    //1. creating a server socket, parameter is local port number
+                    sock = new DatagramSocket(7777);
+
+                    //buffer to receive incoming data
+                    byte[] buffer = new byte[65536];
+                    DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+
+                    //2. Wait for an incoming data
+                    //communication loop
+                    while (true) {
+                        sock.receive(incoming);
+                        byte[] data = incoming.getData();
+                        String message = new String(data, 0, incoming.getLength());
+
+                        //echo the details of incoming data - client ip : client port - client message
+//                echo(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
+                        System.out.println(message);
+                        
+                        //handle the incoming query
+
+                    }
+                } catch (IOException e) {
+                    System.err.println("IOException " + e);
+                }
+                System.out.println("Listening...");
+            }
+        }.start();
+    }
+    
+    public void search(){
+        //search a file
+    }
+}
