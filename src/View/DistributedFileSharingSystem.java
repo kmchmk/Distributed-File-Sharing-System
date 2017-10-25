dffe7afacbdc30f58597864c3c2e690c4e31347b
kmchmk1026 <kmchmk@gmail.com>
2017-10-26 00:38:43 +0530

    Errors fixed.

--- src/View/DistributedFileSharingSystem.java
+++ src/View/DistributedFileSharingSystem.java
@@ -18,7 +18,7 @@ public class DistributedFileSharingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         new Thread() {
             @Override
             public void run() {
@@ -26,10 +26,10 @@ public class DistributedFileSharingSystem {
             }
         }.start();
 
-        NodeImpl node1 = new NodeImpl("One", 3001, 55555);
-        NodeImpl node2 = new NodeImpl("Two", 3002, 55555);
-        NodeImpl node3 = new NodeImpl("Three", 3003, 55555);
-        
+        NodeImpl node1 = new NodeImpl("One", "localhost", 3001, "localhost", 55555);
+        NodeImpl node2 = new NodeImpl("Two", "localhost", 3002, "localhost", 55555);
+        NodeImpl node3 = new NodeImpl("Three", "localhost", 3003, "localhost", 55555);
+
         node1.registerToNetwork();
         node2.registerToNetwork();
         node3.registerToNetwork();
