0a462cf5f0a51c5eab807f3612b3e8cb1a8e425a
kmchmk1026 <kmchmk@gmail.com>
2017-10-25 22:16:39 +0530

    Bootstrap server changed.

--- src/BootstrapServer/BootstrapServer.java
+++ src/BootstrapServer/BootstrapServer.java
@@ -1,5 +1,6 @@
 package BootstrapServer;
 
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -10,21 +11,24 @@ import java.util.StringTokenizer;
 
 public class BootstrapServer {
 
-    public static void main(String args[]) {
+    
+    public static void main(String[] args) {
         runBootstrapServer();
     }
-
-    public static void runBootstrapServer() {
-        DatagramSocket sock;
+    public static void runBootstrapServer()
+    {
+        DatagramSocket sock = null;
         String s;
         List<Neighbour> nodes = new ArrayList<>();
 
-        try {
+        try
+        {
             sock = new DatagramSocket(55555);
 
             echo("Bootstrap Server created at 55555. Waiting for incoming data...");
 
-            while (true) {
+            while(true)
+            {
                 byte[] buffer = new byte[65536];
                 DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
                 sock.receive(incoming);
@@ -40,90 +44,86 @@ public class BootstrapServer {
                 String length = st.nextToken();
                 String command = st.nextToken();
 
-                switch (command) {
-                    case "REG":
-                        {
-                            String reply = "REGOK ";
-                            String ip = st.nextToken();
-                            int port = Integer.parseInt(st.nextToken());
-                            String username = st.nextToken();
-                            if (nodes.isEmpty()) {
-                                reply += "0";
-                                nodes.add(new Neighbour(ip, port, username));
+                if (command.equals("REG")) {
+                    String reply = "REGOK ";
+
+                    String ip = st.nextToken();
+                    int port = Integer.parseInt(st.nextToken());
+                    String username = st.nextToken();
+                    if (nodes.size() == 0) {
+                        reply += "0";
+                        nodes.add(new Neighbour(ip, port, username));
+                    } else {
+                        boolean isOkay = true;
+                        for (int i=0; i<nodes.size(); i++) {
+                            if (nodes.get(i).getPort() == port) {
+                                if (nodes.get(i).getUsername().equals(username)) {
+                                    reply += "9998";
+                                } else {
+                                    reply += "9997";
+                                }
+                                isOkay = false;
+                            }
+                        }
+                        if (isOkay) {
+                            if (nodes.size() == 1) {
+                                reply += "1 " + nodes.get(0).getIp() + " " + nodes.get(0).getPort();
+                            } else if (nodes.size() == 2) {
+                                reply += "2 " + nodes.get(0).getIp() + " " + nodes.get(0).getPort() + " " + nodes.get(1).getIp() + " " + nodes.get(1).getPort();
                             } else {
-                                boolean isOkay = true;
-                                for (int i = 0; i < nodes.size(); i++) {
-                                    if (nodes.get(i).getPort() == port) {
-                                        if (nodes.get(i).getUsername().equals(username)) {
-                                            reply += "9998";
-                                        } else {
-                                            reply += "9997";
-                                        }
-                                        isOkay = false;
-                                    }
+                                Random r = new Random();
+                                int Low = 0;
+                                int High = nodes.size();
+                                int random_1 = r.nextInt(High-Low) + Low;
+                                int random_2 = r.nextInt(High-Low) + Low;
+                                while (random_1 == random_2) {
+                                    random_2 = r.nextInt(High-Low) + Low;
                                 }
-                                if (isOkay) {
-                                    switch (nodes.size()) {
-                                        case 1:
-                                            reply += "1 " + nodes.get(0).getIp() + " " + nodes.get(0).getPort();
-                                            break;
-                                        case 2:
-                                            reply += "2 " + nodes.get(0).getIp() + " " + nodes.get(0).getPort() + " " + nodes.get(1).getIp() + " " + nodes.get(1).getPort();
-                                            break;
-                                        default:
-                                            Random r = new Random();
-                                            int Low = 0;
-                                            int High = nodes.size();
-                                            int random_1 = r.nextInt(High - Low) + Low;
-                                            int random_2 = r.nextInt(High - Low) + Low;
-                                            while (random_1 == random_2) {
-                                                random_2 = r.nextInt(High - Low) + Low;
-                                            }   echo(random_1 + " " + random_2);
-                                            reply += "2 " + nodes.get(random_1).getIp() + " " + nodes.get(random_1).getPort() + " " + nodes.get(random_2).getIp() + " " + nodes.get(random_2).getPort();
-                                            break;
-                                    }
-                                    nodes.add(new Neighbour(ip, port, username));
-                                }
-                            }       reply = String.format("%04d", reply.length() + 5) + " " + reply;
-                            DatagramPacket dpReply = new DatagramPacket(reply.getBytes(), reply.getBytes().length, incoming.getAddress(), incoming.getPort());
+                                echo (random_1 + " " + random_2);
+                                reply += "2 " + nodes.get(random_1).getIp() + " " + nodes.get(random_1).getPort() + " " + nodes.get(random_2).getIp() + " " + nodes.get(random_2).getPort();
+                            }
+                            nodes.add(new Neighbour(ip, port, username));
+                        }
+                    }
+
+                    reply = String.format("%04d", reply.length() + 5) + " " + reply;
+
+                    DatagramPacket dpReply = new DatagramPacket(reply.getBytes() , reply.getBytes().length , incoming.getAddress() , incoming.getPort());
+                    sock.send(dpReply);
+                } else if (command.equals("UNREG")) {
+                    String ip = st.nextToken();
+                    int port = Integer.parseInt(st.nextToken());
+                    String username = st.nextToken();
+                    for (int i=0; i<nodes.size(); i++) {
+                        if (nodes.get(i).getPort() == port) {
+                            nodes.remove(i);
+                            String reply = "0012 UNROK 0";
+                            DatagramPacket dpReply = new DatagramPacket(reply.getBytes() , reply.getBytes().length , incoming.getAddress() , incoming.getPort());
                             sock.send(dpReply);
                             break;
                         }
-                    case "UNREG":
-                        {
-                            String ip = st.nextToken();
-                            int port = Integer.parseInt(st.nextToken());
-                            String username = st.nextToken();
-                            for (int i = 0; i < nodes.size(); i++) {
-                                if (nodes.get(i).getPort() == port) {
-                                    nodes.remove(i);
-                                    String reply = "0012 UNROK 0";
-                                    DatagramPacket dpReply = new DatagramPacket(reply.getBytes(), reply.getBytes().length, incoming.getAddress(), incoming.getPort());
-                                    sock.send(dpReply);
-                                }
-                            }       break;
-                        }
-                    case "ECHO":
-                        {
-                            for (int i = 0; i < nodes.size(); i++) {
-                                echo(nodes.get(i).getIp() + " " + nodes.get(i).getPort() + " " + nodes.get(i).getUsername());
-                            }       String reply = "0012 ECHOK 0";
-                            DatagramPacket dpReply = new DatagramPacket(reply.getBytes(), reply.getBytes().length, incoming.getAddress(), incoming.getPort());
-                            sock.send(dpReply);
-                            break;
-                        }
-                    default:
-                        break;
+                    }
+                } else if (command.equals("ECHO")) {
+                    for (int i=0; i<nodes.size(); i++) {
+                        echo(nodes.get(i).getIp() + " " + nodes.get(i).getPort() + " " + nodes.get(i).getUsername());
+                    }
+                    String reply = "0012 ECHOK 0";
+                    DatagramPacket dpReply = new DatagramPacket(reply.getBytes() , reply.getBytes().length , incoming.getAddress() , incoming.getPort());
+                    sock.send(dpReply);
                 }
 
             }
-        } catch (IOException e) {
+        }
+
+        catch(IOException e)
+        {
             System.err.println("IOException " + e);
         }
     }
 
     //simple function to echo data to terminal
-    public static void echo(String msg) {
+    public static void echo(String msg)
+    {
         System.out.println(msg);
     }
 }
